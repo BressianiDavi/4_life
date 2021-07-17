5a6aa5d1e7102c29642a72972a036ed2ef149a1d
luan da silva alves <luanalves.dev@gmail.com>
2021-07-17 14:26:15 -0300

    fix: header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,43 @@
 <template>
-    <v-container> </v-container>
+  <v-container>
+    <v-app-bar color="deep-purple" app dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>4Life</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Profissio</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: "Header",
+  name: "Header",
 
-    data: () => ({}),
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
 };
 </script>
