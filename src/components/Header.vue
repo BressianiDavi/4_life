bcc6b69288753a893451995b7a9623db835accc4
luan da silva alves <luanalves.dev@gmail.com>
2021-07-17 14:13:04 -0300

    feat: header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,7 +23,7 @@
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Profissio</v-list-item-title>
+            <v-list-item-title>Profissionais</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
