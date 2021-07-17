df4d440238e5584d157d995e4d59816e5b0d7cf4
luan da silva alves <luanalves.dev@gmail.com>
2021-07-17 12:58:41 -0300

    header componente

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-    <v-app></v-app>
+    <v-app>
+        <Header/>
+        <router-view />
+        
+    </v-app>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
     name: "App",
+    components: {
+        Header
+    },
 
     data: () => ({
         //
