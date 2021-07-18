2b144e82d0533b335a01e2742514c2fb8ba04e33
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-18 10:43:43 -0300

    feat: create folder shared to header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
     <v-app>
-        <Header/>
+        <Header />
         <v-main>
             <router-view />
         </v-main>
-        
     </v-app>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from "./components/shared/Header.vue";
 export default {
     name: "App",
     components: {
-        Header
+        Header,
     },
 
     data: () => ({
